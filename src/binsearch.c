90da70ea2b6096c404cc01076d85fb12aa5a2bbb
Joshua Ulrich <josh.m.ulrich@gmail.com>
2018-04-28 19:10:00 -0500

    Add int branch to binsearch
    The binsearch C function now branches on double and integer types, so
    no conversion is done if 'key' and 'vec' are the same type. Cast both
    to double if they're not the same type.
    
    Remove the bound() function and call lower_bound() and upper_bound()
    directly from binsearch(). Also move the edge case checks into the
    respective *_bound() functions, since we need to do the checks inside
    the switch() statement.
    
    Removing the as.double() call means ISO-8601 range-based subsetting
    is nearly twice as fast for integer vectors and keys.
    
    See #100.

--- src/binsearch.c
+++ src/binsearch.c
@@ -5,92 +5,145 @@
 /* Binary search range to find interval contributed by Corwin Joy */
 
 /* Find the smallest index for which A[index] >= key */
-int lower_bound(double key, double *A, int len_A) {
+int lower_bound(SEXP key, SEXP vec)
+{
   int mid;
   int lo = 0;
-  int hi = len_A-1;
-  while (lo < hi) {
-    mid = lo + (hi - lo) / 2;
-    if (A[mid] >= key) {
-      hi = mid;
-    }
-    else {
-      lo = mid + 1;
-    }
+  int hi = length(vec) - 1;
+
+  switch (TYPEOF(vec)) {
+    case REALSXP:
+      {
+        double *A = REAL(vec);
+        double k = REAL(key)[0];
+        while (lo < hi) {
+          mid = lo + (hi - lo) / 2;
+          if (A[mid] >= k) {
+            hi = mid;
+          }
+          else {
+            lo = mid + 1;
+          }
+        }
+        /* start = TRUE, but entire array < key */
+        if (A[lo] < k) {
+          lo = NA_INTEGER;
+        } else {
+          lo++;
+        }
+      }
+      break;
+    case INTSXP:
+      {
+        int *A = INTEGER(vec);
+        int k = INTEGER(key)[0];
+        while (lo < hi) {
+          mid = lo + (hi - lo) / 2;
+          if (A[mid] >= k) {
+            hi = mid;
+          }
+          else {
+            lo = mid + 1;
+          }
+        }
+        /* start = TRUE, but entire array < key */
+        if (A[lo] < k) {
+          lo = NA_INTEGER;
+        } else {
+          lo++;
+        }
+      }
+      break;
+    default:
+      error("unsupported type");
   }
 
   return lo; // lo is the least x for which A[x] >= key is true
 }
 
 /* Find the smallest index for which A[index] > key */
-int upper_bound(double key, double *A, int len_A) {
+int upper_bound(SEXP key, SEXP vec)
+{
   int mid;
   int lo = 0;
-  int hi = len_A - 1;
-  while (lo < hi) {
-    mid = lo + (hi - lo) / 2;
-    if (A[mid] > key) {
-      hi = mid;
-    }
-    else {
-      lo = mid + 1;
-    }
+  int hi = length(vec) - 1;
+
+  switch (TYPEOF(vec)) {
+    case REALSXP:
+      {
+        double *A = REAL(vec);
+        double k = REAL(key)[0];
+        while (lo < hi) {
+          mid = lo + (hi - lo) / 2;
+          if (A[mid] > k) {
+            hi = mid;
+          }
+          else {
+            lo = mid + 1;
+          }
+        }
+        /* if handles edge cases. item may be at the lo/hi end of array */
+        if (lo > 0 && A[lo] > k) lo--;
+
+        /* start = FALSE, but entire array > key */
+        if (A[lo] > k) {
+          lo = NA_INTEGER;
+        } else {
+          lo++;
+        }
+      }
+      break;
+    case INTSXP:
+      {
+        int *A = INTEGER(vec);
+        int k = INTEGER(key)[0];
+        while (lo < hi) {
+          mid = lo + (hi - lo) / 2;
+          if (A[mid] > k) {
+            hi = mid;
+          }
+          else {
+            lo = mid + 1;
+          }
+        }
+        /* handle edge case where item is at the lo/hi end of array */
+        if (lo > 0 && A[lo] > k) lo--;
+
+        /* start = FALSE, but entire array > key */
+        if (A[lo] > k) {
+          lo = NA_INTEGER;
+        } else {
+          lo++;
+        }
+      }
+      break;
+    default:
+      error("unsupported type");
   }
 
   return lo; // lo is the least x for which A[x] > key is true
 }
 
-/* bound the key in the array of vals.
-   if start == true, return lowest index s.t. vals[index] >= key
-   if start == false, return highest index s.t. vals[index] <= key
-*/
-int bound(double key, double *vals, int len_vals, int start) {
-  int item;
-  if (start) {
-    item = lower_bound(key, vals, len_vals);
-  }
-  else {
-    item = upper_bound(key, vals, len_vals);
-    /* if handles edge cases. item may be at the lo/hi end of array */
-    if (item > 0 && vals[item] > key) --item;
-  }
-  return(item);
-}
-
 SEXP binsearch(SEXP key, SEXP vec, SEXP start)
 {
-  double *rvec;
-  double *rkey;
-  int item;
-  int len_vec = length(vec);
-
-  if(!(TYPEOF(vec) == REALSXP && TYPEOF(key) == REALSXP)) {
-    error("both key and vec must be of type double");
-  }
-
-  if(isNull(start)) {
+  if (!isLogical(start)) {
     error("start must be specified as true or false");
   }
 
-  if(len_vec < 1) {
+  if (length(vec) < 1) {
     return ScalarInteger(NA_INTEGER);
   }
 
-  rvec = REAL(vec);
-  rkey = REAL(key);
-  item = bound(*rkey, rvec, len_vec, LOGICAL(start)[0]);
-
-  if(LOGICAL(start)[0]) {
-    if(rvec[item] < *rkey) {
-      /* start = TRUE, but entire array < key */
-      return ScalarInteger(NA_INTEGER);
-    }
+  /* bound the key in the array of vals.
+     if start == true, return lowest index s.t. vals[index] >= key
+     if start == false, return highest index s.t. vals[index] <= key
+  */
+  int item = NA_INTEGER;
+  if (LOGICAL(start)[0]) {
+    item = lower_bound(key, vec);
   } else {
-    if(rvec[item] > *rkey) {
-      /* start = FALSE, but entire array > key */
-      return ScalarInteger(NA_INTEGER);
-    }
+    item = upper_bound(key, vec);
   }
-  return ScalarInteger(item+1);
-}
 
+  return ScalarInteger(item);
+}
